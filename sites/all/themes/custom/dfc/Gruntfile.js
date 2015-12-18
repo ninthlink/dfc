83182eedd154749ad40f83abf53daed4bc8e7891
Troy Shields <tshields@digitalpulp.com>
2015-12-18 04:21:26 -0500

    DFC-16 updated the theme, using grunt for sprites now

--- sites/all/themes/custom/dfc/Gruntfile.js
+++ sites/all/themes/custom/dfc/Gruntfile.js
@@ -2,34 +2,68 @@
 
 module.exports = function (grunt) {
 
+  var PHP_FILES = [
+    '*.info',
+    '{,**}/*.{php,inc}'
+  ];
+  var IMAGE_FILES = [
+    'images/**'
+  ];
+  var IMAGE_FILES_ICONS = [
+    'images/icons2x/*.png'
+  ];
+  var CSS_FILES = [
+    'css/{,**/}*.css'
+  ];
+  var SASS_FILES = [
+    'sass/{,**/}*.{scss,sass}'
+  ];
+  var JS_FILES_NO_MIN = [
+    'js/{,**/}*.js',
+    '!js/{,**/}*.min.js'
+  ];
+
+  // https://github.com/Ensighten/grunt-spritesmith/issues/27
+  // allows for cache busting of sprite images
+  var md5 = require('md5');
+  var fs = require('fs');
+
+  function getCollectiveMd5(files) {
+    var expandedFiles = grunt.file.expand(files),
+        collectiveContent = expandedFiles.reduce(function (content, file) {
+          return content + fs.readFileSync(file, 'binary');
+        }, '');
+    return md5(collectiveContent);
+  }
+
+  var spritecachebust = getCollectiveMd5(IMAGE_FILES_ICONS);
+
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    bower: grunt.file.readJSON('bower.json'),
+    bower_config: grunt.file.readJSON('.bowerrc'),
+
     watch: {
-      options: {
-        livereload: true
-      },
+      options: { },
       sass: {
-        files: ['sass/{,**/}*.{scss,sass}'],
-        tasks: ['compass:dev'],
-        options: {
-          livereload: false
-        }
+        files: SASS_FILES,
+        tasks: ['css:dev']
       },
       registry: {
-        files: ['*.info', '{,**}/*.{php,inc}'],
-        tasks: ['shell'],
-        options: {
-          livereload: false
-        }
+        files: PHP_FILES,
+        tasks: ['shell']
       },
       images: {
-        files: ['images/**']
+        files: IMAGE_FILES
       },
       css: {
-        files: ['css/{,**/}*.css']
+        files: CSS_FILES
       },
       js: {
-        files: ['js/{,**/}*.js', '!js/{,**/}*.min.js'],
-        tasks: ['jshint', 'uglify:dev']
+        files: JS_FILES_NO_MIN,
+        // NOTE: jshint comes before general JS tasks, so it could abort before
+        // general JS tasks are complete.
+        tasks: ['jshint', 'js:dev']
       }
     },
 
@@ -39,21 +73,112 @@ module.exports = function (grunt) {
       }
     },
 
-    compass: {
+    // FIXME: this could be used at some point later to dynamically resize the
+    // 2x icons down to 1x
+    image_resize: {
       options: {
-        config: 'config.rb',
-        bundleExec: true,
-        force: true
+        width: '50%',
+        overwrite: true
       },
-      dev: {
-        options: {
-          environment: 'development'
+      all: {
+        src : IMAGE_FILES_ICONS,
+        dest : 'images/icons/'
+      }
+    },
+
+    // FIXME: this could be used at some point later to combine the retina icons
+    // into a single folder with the normal icons to allow spritesmith to
+    // generate retina sprites in a more conventional way.
+    copy: {
+      all: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'images/icons2x/',
+          dest: 'images/icons/',
+          src: ['*.png'],
+          // rename image.png in src folder to to image-2x.png in dest folder.
+          rename: function(dest, src) {
+            return dest + src.replace(/\.(\w+)$/,'-2x.$1');
+          }
+        }]
+      }
+    },
+
+    sprite:{
+      all: {
+        src             : IMAGE_FILES_ICONS,
+        dest            : 'images/generated/sprite-' + spritecachebust + '.png',
+
+        // FIXME: retina sprite generate is commented out. we have problems with a
+        // few images re: retinaSrcFilter sorting and image size.
+        //
+        // src             : 'images/icons/*.png',
+        // retinaSrcFilter : 'images/icons/*-2x.png',
+        // dest            : 'images/generated/sprite.png',
+        // retinaDest      : 'images/generated/sprite-2x.png',
+
+        destCss         : 'sass/variables/_spritemap.scss',
+        cssFormat       : 'scss_maps',
+
+        // FIXME: need imgPath set explicitly because relative paths resolve
+        // one level up from miss the 'mailman' folder. odd. need to figure out
+        // why.
+        imgPath         : '/sites/all/themes/custom/dfc/images/generated/sprite-' + spritecachebust + '.png',
+        // retinaImgPath   : '/sites/all/themes/custom/mailman/images/generated/sprite-2x.png',
+
+        // same as from compass config
+        algorithm       : "left-right",
+        padding         : 20
+      }
+    },
+
+    sass_globbing: {
+      options: {
+        useSingleQuotes: false
+      },
+      all: {
+        files: {
+          'sass/variables.scss'    : 'sass/variables/**/*.scss',
+          'sass/abstractions.scss' : 'sass/abstractions/**/*.scss',
+          'sass/overrides.scss'    : 'sass/overrides/**/*.scss',
+          'sass/base.scss'         : 'sass/base/**/*.scss',
+          'sass/components.scss'   : 'sass/components/**/*.scss'
+        }
+      }
+    },
+
+    sass: {
+      // see https://github.com/sass/node-sass#options
+      options: {
+        sourceMap: true,
+        outputStyle : 'nested', // nested, expanded, compact, compressed
+        sourceComments : true,
+        // bower packages @import'ed in our CSS
+        // FIXME: avoid hardcoding all of this. (thought: IIFE + somehow
+        // leverage bower.json)
+        includePaths : [
+          "<%= bower_config.directory %>/normalize-scss",
+          "<%= bower_config.directory %>/compass-mixins/lib",
+          "<%= bower_config.directory %>/breakpoint-sass/stylesheets",
+          "<%= bower_config.directory %>/susy/sass"
+        ]
+      },
+      dev : {
+        options : { },
+        files: {
+          'css/dfc.hacks.css'    : 'sass/dfc.hacks.scss',
+          'css/dfc.no-query.css' : 'sass/dfc.no-query.scss',
+          'css/dfc.normalize.css': 'sass/dfc.normalize.scss',
+          'css/dfc.styles.css'   : 'sass/dfc.styles.scss'
         }
       },
-      dist: {
-        options: {
-          environment: 'production'
-        }
+      dist : {
+        options : {
+          outputStyle : 'compressed',
+          sourceComments : false
+        },
+        files: "<%= sass.dev.files %>"
       }
     },
 
@@ -61,7 +186,7 @@ module.exports = function (grunt) {
       options: {
         jshintrc: '.jshintrc'
       },
-      all: ['js/{,**/}*.js', '!js/{,**/}*.min.js']
+      all: JS_FILES_NO_MIN
     },
 
     uglify: {
@@ -74,9 +199,8 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           flatten: true,
-          cwd: 'js',
           dest: 'js',
-          src: ['**/*.js', '!**/*.min.js'],
+          src: JS_FILES_NO_MIN,
           rename: function(dest, src) {
             var folder = src.substring(0, src.lastIndexOf('/'));
             var filename = src.substring(src.lastIndexOf('/'), src.length);
@@ -90,33 +214,90 @@ module.exports = function (grunt) {
           mangle: true,
           compress: true
         },
-        files: [{
-          expand: true,
-          flatten: true,
-          cwd: 'js',
-          dest: 'js',
-          src: ['**/*.js', '!**/*.min.js'],
-          rename: function(dest, src) {
-            var folder = src.substring(0, src.lastIndexOf('/'));
-            var filename = src.substring(src.lastIndexOf('/'), src.length);
-            filename = filename.substring(0, filename.lastIndexOf('.'));
-            return dest + '/' + folder + filename + '.min.js';
-          }
-        }]
+        files: "<%= uglify.dev.files %>"
       }
+    },
+
+    notify_hooks: {
+      options: {
+        enabled: true,
+        success: true
+      }
+    }
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-sass-globbing');
+  grunt.loadNpmTasks('grunt-spritesmith');
+  grunt.loadNpmTasks('grunt-image-resize');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-notify');
+
+  grunt.task.run('notify_hooks');
+
+  grunt.registerTask('css', 'Run all tasks related to CSS compilation.', function(target) {
+    // expect target passed. if no target, pull it from GRUNT_TARGET, otherwise
+    // default to 'dev'
+    if (target === undefined) {
+      target = process.env['GRUNT_TARGET'] || 'dev';
+      console.log("No target passed. Defaulting to '" + target + "'.");
+    }
+
+    grunt.task.run(
+      // FIXME: add these tasks back in once we have retina sprite generation
+      // worked out
+      // 'image_resize',
+      // 'copy',
+      'sprite',
+      'sass_globbing',
+      'sass:' + target
+    );
+  });
+
+  grunt.registerTask('js', 'Run all tasks related to JS (linting, minification).', function(target) {
+    // expect target passed. if no target, pull it from GRUNT_TARGET, otherwise
+    // default to 'dev'
+    if (target === undefined) {
+      target = process.env['GRUNT_TARGET'] || 'dev';
+      console.log("No target passed. Defaulting to '" + target + "'.");
+    }
+
+    if (target === "dev") {
+      // run jshint after uglify so the task does not abort due to linting
+      // issues. it should really abort, but let's not change horses midstream.
+      //
+      // FIXME: disabled jshint because we don't seem to pay attention to
+      // jshint's output, and should we switch to 'dev' on CI for any reason,
+      // the grunt task should not abort.
+      grunt.task.run(
+        'uglify:' + target
+        // 'jshint'
+      );
+    } else {
+      grunt.task.run(
+        'uglify:' + target
+      );
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-shell');
+  grunt.registerTask('build', 'Run all front-end related build tasks.', function(target) {
+    // expect target passed. if no target, pull it from GRUNT_TARGET, otherwise
+    // default to 'dev'
+    if (target === undefined) {
+      target = process.env['GRUNT_TARGET'] || 'dev';
+      console.log("No target passed. Defaulting to '" + target + "'.");
+    }
 
-  grunt.registerTask('build', [
-    'uglify:dist',
-    'compass:dist',
-    'jshint'
-  ]);
+    grunt.task.run(
+      'css:' + target,
+      'js:' + target
+    );
+  });
 
 };
--- sites/all/themes/custom/dfc/config.rb
+++ sites/all/themes/custom/dfc/config.rb
@@ -22,7 +22,6 @@ generated_images_dir = images_dir + "/generated"
 javascripts_dir = "js"
 
 # Require any additional compass plugins installed on your system.
-require 'compass-normalize'
 require 'rgbapng'
 require 'toolkit'
 require 'breakpoint'

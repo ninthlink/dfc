0e0a0b8e091b0796e42ce0db619541287f92279f
Andrew Root <andrew.b.root@gmail.com>
2016-02-05 10:09:35 -0800

    points on product views

--- sites/all/themes/custom/dfc/js/product.js
+++ sites/all/themes/custom/dfc/js/product.js
@@ -1,25 +1,28 @@
 (function ($) {
   Drupal.behaviors.productImages = {
     attach: function (context, settings) {
+      var width = '535px';
       $('#main-image').html($('.field--product-image img:first').clone());
 
       $('.field--product-image img').click(function () {
         $('#main-image').html($(this).clone());
+        $('#main-image img').width(width);
       });
 
       $('.images-enlarge').click(function() {
         var img = $('#main-image img');
         var container = $('.main-content');
-        var width = '535px';
         if (!container.hasClass('expanded')) {
-          container.addClass('expanded').width('100%');
-          img.width('100%');
+          width = '100%';
+          container.addClass('expanded')
           $(this).text('- reduce');
         } else {
-          container.removeClass('expanded').width(width);
-          img.width(width);
+          width = '535px';
+          container.removeClass('expanded')
           $(this).text('+ enlarge');
         }
+        container.width(width);
+        img.width(width);
       });
     }
   };
